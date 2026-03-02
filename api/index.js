c19d3e39f160fffe4ddeae189d05105e30c5ec9a
Beryl <beryl@lakoo.com>
2026-03-02 14:26:40 +0800

    Use single workspace config

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-// Correct Slack Bolt import
+// Slack Bolt for single workspace
 let appInstance = null;
 
 async function getApp() {
@@ -7,9 +7,6 @@ async function getApp() {
     appInstance = new App({
       signingSecret: process.env.SLACK_SIGNING_SECRET,
       token: process.env.SLACK_BOT_TOKEN,
-      clientId: process.env.SLACK_CLIENT_ID,
-      clientSecret: process.env.SLACK_CLIENT_SECRET,
-      stateSecret: process.env.SLACK_STATE_SECRET,
     });
   }
   return appInstance;
