fcd7a90c66043649d0a73cbdcf5a8218961fa35d
Beryl <beryl@lakoo.com>
2026-03-02 12:55:08 +0800

    Simplify API endpoints for testing

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-// Simple test endpoint
 export default function handler(req, res) {
-  res.json({ status: 'ok', message: 'TR Announcement Bot is running' });
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).json({ hello: 'world' });
 }
